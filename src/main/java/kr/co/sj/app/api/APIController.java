b34ddbae1bfacf97698ce0bee9e63bc1aea20f15
hwan <hwani6865@whalesoft.co.kr>
2025-09-23 17:38:17 +0900

    Feat : 룸패드 API 기능 추가

--- src/main/java/kr/co/sj/app/api/APIController.java
+++ src/main/java/kr/co/sj/app/api/APIController.java
@@ -1,48 +1,64 @@
 package kr.co.sj.app.api;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
+import java.util.List;
+import java.util.ResourceBundle;
 
 @Controller
 @RequestMapping(value = {"/api/"})
 public class APIController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping(value = {"circlesRoomUser.*"})
-	public @ResponseBody ResponseEntity<String> circlesRoomUser(String data, String facility_id, HttpServletRequest request) throws Exception {
+	public final static String CIRCLES_API_URL = "http://218.48.151.16:8010/api/circlesRoomUser.do";
+
+	@RequestMapping(
+    value = {"circlesRoomUser.*"}, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String circlesRoomUser(@RequestParam(value = "data", required = false) String data,
+								  @RequestParam(value = "circles_div", required = false) String circlesDiv,
+								  HttpServletRequest request) throws Exception {
 		RestTemplate restTemplate = new RestTemplate();
+		List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
+		for (int i = 0; i < converters.size(); i++) {
+			if (converters.get(i) instanceof StringHttpMessageConverter) {
+				converters.set(i, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+			}
+		}
 
-        String apiUrl = "https://외부API주소?facility_id=" + facility_id;
+		StringBuilder apiUrl = new StringBuilder(CIRCLES_API_URL);
+		boolean hasParam = false;
 
-        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+		if (StringUtils.isNotEmpty(data)) {
+			apiUrl.append(hasParam ? "&" : "?").append("data=").append(URLEncoder.encode(data, "UTF-8"));
+			hasParam = true;
+		}
 
-		log.info("@@@@@@@@@@@@@@@ circlesRoomUserList log");
-		log.info("URL : " + request.getRequestURL() +"?"+ request.getQueryString());
-		log.info("Result : " + response.getStatusCode());
+		if (StringUtils.isNotEmpty(circlesDiv)) {
+			apiUrl.append(hasParam ? "&" : "?").append("circles_div=").append(URLEncoder.encode(circlesDiv, "UTF-8"));
+			hasParam = true;
+		}
 
-		return response;
+		ResponseEntity<String> response = restTemplate.getForEntity(apiUrl.toString(), String.class);
+
+		log.info("@@@@@@@@@@@@@@@ circlesRoomUser log : " + request.getRequestURL() + "?" + request.getQueryString());
+		log.info("CALL URL : " + apiUrl);
+
+		return response.getBody();
 	}
 
-	@RequestMapping(value = "circlesRoomUserList.*")
-	public @ResponseBody ResponseEntity<String> circlesRoomUserList(String facility_id, String reserve_date, HttpServletRequest request) throws ParseException {
-		RestTemplate restTemplate = new RestTemplate();
-
-        String apiUrl = "https://외부API주소?facility_id=" + facility_id + "&reserve_date=" + reserve_date;
-
-        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
-
-		log.info("@@@@@@@@@@@@@@@ circlesRoomUserList log");
-		log.info("URL : " + request.getRequestURL() +"?"+ request.getQueryString());
-		log.info("Result : " + response.getStatusCode());
-
-		return response;
-	}
 }
