6a9c1ad9f4b50b392b54ec2c83e644794197c65c
happygxxg <kangyoonkyung@infoset.co.kr>
2025-09-25 09:04:37 +0900

    style : 에티켓 슬라이드 추가

--- src/main/webapp/resources/ict/dglib/media/js/stairs5.js
+++ src/main/webapp/resources/ict/dglib/media/js/stairs5.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
             $slider.slick({
                 slidesToShow: 1,
                 arrows: false,
-                autoplay: false,
+                autoplay: true,
                 autoplaySpeed: 8000,
                 dots: false,
                 infinite: true,
